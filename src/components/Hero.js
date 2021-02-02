ded822a6b9e1494e23faa83bfe03e2d7aeb7bff5
Dogukan <dogukangulyasar@gmail.com>
2021-02-02 21:46:56 +0300

    Little revision for the hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,20 +5,18 @@ import Space from "../Assets/img/space.svg";
 export class Hero extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row hero">
-          <div className="col-6 hero-text">
-            <h2 className="hero-title">Buradayız, </h2>
-            <h2 className="hero-yeniyiz">Yeniyiz</h2>
-            <h2 className="hero-birlikteyiz">Birlikteyiz.</h2>
-            <h4 className="hero-parag">
-              Teknolojinin getirdiği imkanları ve kolaylıkları çevremize
-              sunabilmek için çalışıyoruz.
-            </h4>
-          </div>
-          <div className="col-6 hero-image">
-            <img className="hero-space" src={Space} />
-          </div>
+      <div className="row hero">
+        <div className="col-6 hero-text">
+          <h1 className="hero-title">Buradayız, </h1>
+          <h1 className="hero-yeniyiz">Yeniyiz</h1>
+          <h1 className="hero-birlikteyiz">Birlikteyiz.</h1>
+          <h4 className="hero-parag">
+            Teknolojinin getirdiği imkanları ve kolaylıkları çevremize
+            sunabilmek için çalışıyoruz.
+          </h4>
+        </div>
+        <div className="col-6 hero-image">
+          <img className="hero-space" src={Space} alt="space animation" />
         </div>
       </div>
     );
